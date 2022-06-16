bcb11d8bcfd566362350efbec50c888ab8ba51e8
Adam Li <adam2392@gmail.com>
2022-06-16 11:17:15 -0400

    Fix unit test

--- causal_networkx/graphs/tests/test_graphs.py
+++ causal_networkx/graphs/tests/test_graphs.py
@@ -12,18 +12,17 @@ from causal_networkx.io import load_from_networkx
 from causal_networkx.utils import requires_pydot
 
 
-def test_spouses(self):
+def test_spouses():
     ed1, ed2 = ({}, {})
     incoming_graph_data = {0: {1: ed1, 2: ed2}}
     G = DAG(incoming_graph_data)
     # add collider on 2
     G.add_edge(3, 2)
 
-    assert G.spouses(0) == (3,)
-    assert G.spouses(1) == {}
-    assert G.spouses(3) == {
-        0,
-    }
+    assert G.spouses(0) == {3}
+    assert G.spouses(1) == set()
+    assert G.spouses(2) == set()
+    assert G.spouses(3) == {0}
 
 
 class TestDAG:
